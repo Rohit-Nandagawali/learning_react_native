e2e52fc07ce92c83e034041bfb34f42a557b02f1
Rohit Nandagawali <nandagawalirohit143@gmail.com>
2023-02-21 00:57:59 +0530

    played with button

--- App.tsx
+++ App.tsx
@@ -6,23 +6,23 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
   View,
+  Button
 } from 'react-native';
 
 
 function App(): JSX.Element {
+  function handlePress() {
+      console.warn("someone pressed me")
+      
+  }
   return (
-    <View>
+    <View style={{flex:1,alignItems:"center",justifyContent:"center"}}>
       <Text style={{fontSize:40,fontWeight:'bold' }}>Hello World !</Text>
       <Text style={{color:"#0000ff", fontSize:20}}>This app is created by Rohit Nandagawali</Text>
+      <Button title='Press Me' onPress={handlePress}></Button>
     </View>
   );
 }
