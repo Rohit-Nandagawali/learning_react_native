bb8f5fd4dcfa92e15155a431b75903cbac60c2d9
Rohit Nandagawali <nandagawalirohit143@gmail.com>
2023-02-21 01:28:51 +0530

    learned about props
    props are the properties to the components

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import {
   Text,
   View,
@@ -17,18 +17,32 @@ function App(): JSX.Element {
   const [name, setName] = useState("Rohit")
 
   function handlePress() {
-    name==="Rohit"?setName("User"):setName("Rohit")
-      // console.warn("someone pressed me")
-      
+    name === "Rohit" ? setName("User") : setName("Rohit")
+    // console.warn("someone pressed me")
+
   }
   return (
-    <View style={{flex:1,alignItems:"center",justifyContent:"center"}}>
-      <Text style={{fontSize:40,fontWeight:'bold' }}>Hello {name}</Text>
-      <Text style={{color:"#0000ff", fontSize:20}}>This app is created by Rohit Nandagawali</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={{ fontSize: 40, fontWeight: 'bold' }}>Hello {name}</Text>
+      <Text style={{ color: "#0000ff", fontSize: 20 }}>This app is created by Rohit Nandagawali</Text>
       <Button title='Change name' onPress={handlePress}></Button>
+
+      <User name="Rohit" age={50}/>
     </View>
   );
 }
+type data={
+  name:String,
+  age:Number
+}
+const User = (props:data) => {
+  return (
 
+    <View style={{ backgroundColor: "pink", padding: 5, margin: 5 }}>
+      <Text>Name : { props.name}</Text>
+      
+    </View>
+  )
+}
 
 export default App;
