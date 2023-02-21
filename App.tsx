7566ffce08227c257ee37f4c73fce5c4c3c6b2ef
Rohit Nandagawali <nandagawalirohit143@gmail.com>
2023-02-21 13:44:22 +0530

    created a form using states
    learn about  secureTextEntry=boolean
    is used to create password field, and practiced previous concepts

--- App.tsx
+++ App.tsx
@@ -5,30 +5,69 @@ import externalStyle from './externalStyle';
 import {
   Text,
   View,
-  Button,
   StyleSheet,
   TextInput,
   Pressable
 } from 'react-native';
 
 
+
 function App(): JSX.Element {
-  const [name,setName]=useState("")
+  const [name, setName] = useState("")
+  const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("")
+  const [display, setDisplay] = useState(false)
+
+  const handleClear=()=>{
+    setDisplay(false)
+    setName("")
+    setPassword("")
+    setEmail("")
+  }
+
   return (
-    <View style={{flex:1,alignItems:"center",justifyContent:"center"}}>
-      <Text style={{ fontSize: 30, fontWeight: 'bold',margin:10}}>Your Name is {name}</Text>
-      <TextInput 
-      style={Internalstyles.TextInput}
-      value={name}
-      placeholder='Enter Your name'
-      onChangeText={(text)=>setName(text)}
+    <View >
+      <Text style={{ fontSize: 30, fontWeight: 'bold', marginTop: 40, alignSelf: 'center' }}>Enter Your data</Text>
+      <TextInput
+        style={Internalstyles.TextInput}
+        value={name}
+        placeholder='Enter Your Name'
+        onChangeText={(text) => setName(text)}
+      />
+      <TextInput
+        style={Internalstyles.TextInput}
+        value={password}
+        secureTextEntry={true}
+        placeholder='Enter Password'
+        onChangeText={(text) => setPassword(text)}
+      />
+      <TextInput
+        style={Internalstyles.TextInput}
+        value={email}
+        placeholder='Enter email'
+        onChangeText={(text) => setEmail(text)}
       />
 
-      <Pressable style={Internalstyles.Button} onPress={()=>setName("")}>
-        <Text style={{fontSize:20}}>Clear</Text>
+      <Pressable style={[Internalstyles.Button]} onPress={() => {setName("") ;setDisplay(true)}}>
+        <Text style={{ fontSize: 20, color: "white", fontFamily: "Roboto", }}>Submit</Text>
       </Pressable>
 
-    {/* there is no style property to <Button> use <Presseble> */}
+      <Pressable style={[Internalstyles.Button, { backgroundColor: "red" }]} onPress={handleClear}>
+        <Text style={{ fontSize: 20, color: "white", fontFamily: "Roboto", }}>Clear</Text>
+      </Pressable>
+
+      {display ?
+        <View>
+          <Text style={{ fontSize: 20 }}>
+            Your name is {name},
+            Your password is {password},
+            Your email is {email}
+          </Text>
+        </View> : null
+      }
+
+
+      {/* there is no style property to <Button> use <Presseble> */}
       {/* <Button 
       title='Clear'
       onPress={()=>setName("")}/> */}
@@ -39,30 +78,30 @@ function App(): JSX.Element {
 
 
 // defining Internal styling using StyleSheet.create()
-const Internalstyles=StyleSheet.create({
-  TextInput:{
-    color:"white",
-    fontSize:20,
-    fontFamily:"Roboto",
-    padding:15,
-    width:300,
-    borderRadius:20,
-    borderWidth:2,
-    borderColor:"green"
+const Internalstyles = StyleSheet.create({
+  TextInput: {
+    color: "black",
+    fontSize: 20,
+    fontFamily: "Roboto",
+    padding: 15,
+    marginHorizontal: 20,
+    marginVertical: 10,
+    borderRadius: 20,
+    borderWidth: 2,
+    borderColor: "green"
   }
   ,
-  Button:{
-    color:"white",
-    fontSize:25,
-    fontFamily:"Roboto",
-    padding:15,
-    margin:10,
-    backgroundColor:"green",
-    borderRadius:20,
-    alignItems:"center",
-    textAlign:"center",
-    borderColor:"green"
-  }
-  
+  Button: {
+
+    padding: 15,
+    margin: 10,
+    backgroundColor: "green",
+    borderRadius: 20,
+    alignItems: "center",
+    textAlign: "center",
+    borderColor: "green",
+
+  },
+
 })
 export default App;
